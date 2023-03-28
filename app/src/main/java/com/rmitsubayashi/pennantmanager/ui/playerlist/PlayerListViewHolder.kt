25364ec62664a22ebe53ae431aab7672dea83655
rmitsubayashi <ryomitsu@gmail.com>
2023-03-28 11:09:29 +0900

    order positions on player list

--- app/src/main/java/com/rmitsubayashi/pennantmanager/ui/playerlist/PlayerListViewHolder.kt
+++ app/src/main/java/com/rmitsubayashi/pennantmanager/ui/playerlist/PlayerListViewHolder.kt
@@ -4,6 +4,7 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.rmitsubayashi.pennantmanager.R
 import com.rmitsubayashi.pennantmanager.data.model.Player
+import com.rmitsubayashi.pennantmanager.data.model.Position
 import com.rmitsubayashi.pennantmanager.databinding.ItemPlayerListBinding
 import com.rmitsubayashi.pennantmanager.ui.util.PositionStringMapper
 
@@ -11,7 +12,14 @@ class PlayerListViewHolder(private val viewModel: PlayerListViewModel, private v
     fun setPlayer(player: Player, contextMenuListener: View.OnCreateContextMenuListener) {
         binding.nameTextview.text = player.name
         binding.ageTextview.text = binding.root.context.getString(R.string.age, player.age)
-        binding.positionsTextview.text = player.positions.joinToString("") {
+        // order of positions should be uniform
+        val orderedPlayerPositions = mutableListOf<Position>()
+        for (p in Position.values()) {
+            if (p in player.positions) {
+                orderedPlayerPositions.add(p)
+            }
+        }
+        binding.positionsTextview.text = orderedPlayerPositions.joinToString("") {
             val resId = PositionStringMapper.mapPositionToAbbreviationStringRes(it)
             binding.root.context.getString(resId)
         }
